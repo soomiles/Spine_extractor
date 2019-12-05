575af393f08f247141116b2baeab243b7482bf12
Soo95 <plmko32@gmail.com>
2019-12-05 22:18:45 +0900

    pointcapsnet-ae

---
+++ ModelNetCaps/Learning.py
@@ -0,0 +1,218 @@
+from pathlib import Path
+import pandas as pd
+import torch
+from tqdm import tqdm
+from scripts.utils import empty_cuda_cache, reduce_tensor
+import heapq
+import torch.nn.functional as F
+from torchvision.utils import save_image
+import pdb
+
+class Learning():
+    def __init__(self,
+                 distrib_config,
+                 optimizer,
+                 loss_fn,
+                 device,
+                 n_epoches,
+                 scheduler,
+                 accumulation_step,
+                 early_stopping,
+                 logger,
+                 tb_logger,
+                 best_checkpoint_folder,
+                 checkpoints_history_folder,
+                 checkpoints_topk,
+                 calculation_name
+        ):
+        self.distrib_config = distrib_config
+        self.logger = logger
+        self.tb_logger = tb_logger
+        self.optimizer = optimizer
+        self.loss_fn = loss_fn
+        self.device = device
+        self.epoch = 0
+        self.n_epoches = n_epoches
+        self.scheduler = scheduler
+        self.accumulation_step = accumulation_step
+        self.early_stopping = early_stopping
+        self.calculation_name = calculation_name
+        self.best_checkpoint_path = Path(
+            best_checkpoint_folder,
+            '{}.pth'.format(self.calculation_name)
+        )
+        self.checkpoints_history_folder = checkpoints_history_folder
+        self.score_heap = [(0., Path('nothing'))]
+        self.summary_file = Path(self.checkpoints_history_folder, 'summary.csv')
+        if self.summary_file.is_file():
+            self.best_score = pd.read_csv(self.summary_file).best_metric.min()
+            if self.distrib_config['LOCAL_RANK'] == 0:
+                logger.info('Pretrained best score is {:.5}'.format(self.best_score))
+        else:
+            self.best_score = 0
+        self.best_score = 0
+        self.best_epoch = -1
+        self.checkpoints_topk = checkpoints_topk
+        # empty_cuda_cache()
+
+    def train_epoch(self, model, loader):
+        tqdm_loader = tqdm(loader)
+        current_loss_mean = 0.
+        for idx, batch in enumerate(tqdm_loader):
+            if (idx + 1) % self.accumulation_step == 0:
+                self.optimizer.zero_grad()
+
+            loss = self.batch_train(model, batch)
+            current_loss_mean = (current_loss_mean * idx + loss.item()) / (idx + 1)
+
+            if (idx + 1) % self.accumulation_step == 0:
+                self.optimizer.step()
+
+            tqdm_loader.set_description(f'loss: {current_loss_mean:.4f} lr: {self.optimizer.param_groups[0]["lr"]:.6f}')
+        # empty_cuda_cache()
+        return current_loss_mean
+
+    def batch_train(self, model, batch):
+        batch_graphs = batch.to(device=self.device)
+        batch_graphs = torch.stack(list(map(lambda x: x.pos, batch_graphs.to_data_list())))
+        batch_graphs = batch_graphs.transpose(2, 1).contiguous()
+        codewords, reconstruction = model(batch_graphs)
+
+        batch_graphs_ = batch_graphs.transpose(2, 1).contiguous()
+        reconstruction_ = reconstruction.transpose(2, 1).contiguous()
+        loss = self.loss_fn(batch_graphs_, reconstruction_) / self.accumulation_step
+
+        loss.backward()
+        # with amp.scale_loss(loss, self.optimizer, loss_id=0) as scaled_loss:
+        #     scaled_loss.backward()
+        return loss
+
+    def valid_epoch(self, model, loader, local_metric_fn):
+        tqdm_loader = tqdm(loader)
+        current_loss_mean = torch.tensor(0.).cuda()
+        for idx, batch in enumerate(tqdm_loader):
+            with torch.no_grad():
+                batch_loss = self.batch_valid(model, batch)
+                current_loss_mean = (current_loss_mean * idx + batch_loss) / (idx + 1)
+
+                tqdm_loader.set_description(f'val_loss: {current_loss_mean:.5f}')
+
+        if self.distrib_config['DISTRIBUTED']:
+            current_loss_mean = reduce_tensor(current_loss_mean, self.distrib_config['WORLD_SIZE'])
+        # empty_cuda_cache()
+        return current_loss_mean.item()
+
+    def batch_valid(self, model, batch):
+        batch_graphs = batch.to(device=self.device)
+        batch_graphs = torch.stack(list(map(lambda x: x.pos, batch_graphs.to_data_list())))
+        batch_graphs = batch_graphs.transpose(2, 1).contiguous()
+        codewords, reconstruction = model(batch_graphs)
+
+        batch_graphs_ = batch_graphs.transpose(2, 1).contiguous()
+        reconstruction_ = reconstruction.transpose(2, 1).contiguous()
+        loss = self.loss_fn(batch_graphs_, reconstruction_) / self.accumulation_step
+        return loss.item()
+
+    def process_summary(self, valid_score_mean):
+        self.logger.info('{} epoch: \t start searching thresholds....'.format(self.epoch))
+        # selected_score, thr = global_metric_fn(eval_list)
+        selected_score = valid_score_mean
+
+        epoch_summary = pd.DataFrame(
+            data=[[self.epoch, selected_score]],
+            columns=['epoch', 'best_metric']
+        )
+
+        if self.distrib_config['LOCAL_RANK'] == 0:
+            self.logger.info(f'Epoch {self.epoch}: \t Calculated score: {selected_score:.6f}')
+            self.tb_logger.add_scalar('Valid/score', selected_score, self.epoch)
+
+            if not self.summary_file.is_file():
+                epoch_summary.to_csv(self.summary_file, index=False)
+            else:
+                summary = pd.read_csv(self.summary_file)
+                summary = summary.append(epoch_summary)
+                summary.to_csv(self.summary_file, index=False)
+
+        return selected_score
+
+    @staticmethod
+    def get_state_dict(model):
+        if type(model) == torch.nn.DataParallel:
+            state_dict = model.module.state_dict()
+        else:
+            state_dict = model.state_dict()
+        return state_dict
+
+    def post_processing(self, score, model):
+        if score < self.best_score:
+            self.best_score = score
+            self.best_epoch = self.epoch
+            if self.distrib_config['LOCAL_RANK'] == 0:
+                torch.save(self.get_state_dict(model), self.best_checkpoint_path)
+                self.logger.info('best model: {} epoch - {:.5}'.format(self.epoch, score))
+
+        # if self.distrib_config['LOCAL_RANK'] == 0:
+        #     if self.score_heap[0][0] < score:
+        #         checkpoints_history_path = Path(
+        #             self.checkpoints_history_folder,
+        #             '{}_epoch{}.pth'.format(self.calculation_name, self.epoch)
+        #         )
+        #         torch.save(self.get_state_dict(model), checkpoints_history_path)
+        #         heapq.heappush(self.score_heap, (score, checkpoints_history_path))
+        #         if len(self.score_heap) > self.checkpoints_topk:
+        #             _, removing_checkpoint_path = heapq.heappop(self.score_heap)
+        #             removing_checkpoint_path.exists() and removing_checkpoint_path.unlink()
+        #             self.logger.info('Removed checkpoint is {}'.format(removing_checkpoint_path))
+
+        if self.scheduler.__class__.__name__ == 'ReduceLROnPlateau':
+            self.scheduler.step(score)
+        else:
+            self.scheduler.step()
+
+    def inference(self, model, loader):
+        empty_cuda_cache()
+        state_dict = torch.load(self.best_checkpoint_path)
+        model.load_state_dict(state_dict)
+        tqdm_loader = tqdm(loader)
+        for idx, batch in enumerate(tqdm_loader):
+            with torch.no_grad():
+                batch_imgs = batch[0].to(device=self.device, non_blocking=True)
+                batch_pred = model(batch_imgs).cpu()
+                batch_pred = F.softmax(batch_pred, dim=1)[:, 1, ...]
+                for pred_idx, pred in enumerate(batch_pred):
+                    save_image(pred, self.checkpoints_history_folder / f'{pred_idx}.tif')
+
+    def run_train(self, model, train_dataloader, valid_dataloader, local_metric_fn, global_metric_fn):
+        model.to(self.device)
+        # model, self.optimizer = amp.initialize(model, self.optimizer, opt_level='O1')
+        for self.epoch in range(self.n_epoches):
+            if self.distrib_config['LOCAL_RANK'] == 0:
+                self.logger.info(f'Epoch {self.epoch}: \t start training....')
+            model.train()
+            train_loss_mean = self.train_epoch(model, train_dataloader)
+            if self.distrib_config['LOCAL_RANK'] == 0:
+                self.logger.info(f'Epoch {self.epoch}: \t Calculated train loss: {train_loss_mean:.5f}')
+                self.tb_logger.add_scalar('Train/Loss', train_loss_mean)
+
+            if self.distrib_config['LOCAL_RANK'] == 0:
+                self.logger.info(f'Epoch {self.epoch}: \t start validation....')
+            model.eval()
+            valid_score_mean = self.valid_epoch(model, valid_dataloader, local_metric_fn)
+            selected_score = self.process_summary(valid_score_mean)
+
+            self.post_processing(selected_score, model)
+
+            if self.epoch - self.best_epoch > self.early_stopping:
+                if self.distrib_config['LOCAL_RANK'] == 0:
+                    self.logger.info('EARLY STOPPING')
+                break
+
+        if self.distrib_config['LOCAL_RANK'] == 0:
+            self.tb_logger.close()
+
+        # self.inference(model, valid_dataloader)
+        empty_cuda_cache()
+        return self.best_epoch, self.best_score
+
+
---
+++ ModelNetCaps/Train.py
@@ -0,0 +1,210 @@
+import argparse
+import os
+import sys
+from pathlib import Path
+from scripts.utils import load_yaml, seed_everything, init_logger
+from scripts.tb_helper import init_tb_logger
+from scripts.metric import apply_deep_thresholds, search_deep_thresholds
+from torch_geometric.datasets import ModelNet
+from scripts.ChamferLoss import ChamferLoss
+from ModelNetCaps.Learning import Learning
+from torch_geometric.data import DataLoader
+import torch
+import torch_geometric.transforms as T
+import functools
+import importlib
+from apex.parallel import DistributedDataParallel, convert_syncbn_model
+
+sys.path.append('/workspace/lib/segmentation_models.pytorch')
+sys.path.append('/workspace/lib/utils')
+import radam
+
+class SingleFromMultipleLoadError(Exception):
+    def __str__(self):
+        return "SingleFromMultipleLoadError"
+
+class MultipleFromSingleLoadError(Exception):
+    def __str__(self):
+        return "MultipleFromSingleLoadError"
+
+def argparser():
+    parser = argparse.ArgumentParser(description='ModelNet self-supervised learning')
+    parser.add_argument('--config_path', type=str, help='train config path')
+    parser.add_argument('--local_rank', type=int, default=0, help='for distributed')
+    return parser.parse_args()
+
+
+def init_eval_fns(train_config):
+    search_pairs = train_config['EVALUATION']['SEARCH_PAIRS']
+    n_search_workers = train_config.get('WORKERS', 1)
+    # n_search_workers = -2
+
+    thrs_list = [thr for thr in search_pairs]
+
+    local_metric_fn = functools.partial(
+        apply_deep_thresholds,
+    )
+
+    global_metric_fn = functools.partial(
+        search_deep_thresholds,
+        thrs_list=thrs_list,
+        n_search_workers=n_search_workers
+    )
+    return local_metric_fn, global_metric_fn
+
+
+def train_fold(
+    train_config, distrib_config, pipeline_name, log_dir, fold_id,
+    train_dataloader, valid_dataloader,
+    local_metric_fn, global_metric_fn):
+
+    if distrib_config['LOCAL_RANK'] == 0:
+        fold_logger = init_logger(log_dir, 'train_fold_{}.log'.format(fold_id))
+        fold_tb_logger = init_tb_logger(log_dir, 'train_fold_{}'.format(fold_id))
+
+    best_checkpoint_folder = Path(log_dir, train_config['CHECKPOINTS']['BEST_FOLDER'])
+    best_checkpoint_folder.mkdir(exist_ok=True, parents=True)
+
+    checkpoints_history_folder = Path(
+        log_dir,
+        train_config['CHECKPOINTS']['FULL_FOLDER'],
+        'fold{}'.format(fold_id)
+    )
+    checkpoints_history_folder.mkdir(exist_ok=True, parents=True)
+    checkpoints_topk = train_config['CHECKPOINTS']['TOPK']
+
+    calculation_name = '{}_fold{}'.format(pipeline_name, fold_id)
+
+    device = train_config['DEVICE']
+
+    module = importlib.import_module(train_config['MODEL']['PY'])
+    model_function = getattr(module, train_config['MODEL']['CLASS'])
+    model = model_function(**train_config['MODEL']['ARGS'])
+
+    if len(train_config['DEVICE_LIST']) > 1:
+        model.cuda()
+        model = convert_syncbn_model(model)
+        model = DistributedDataParallel(model, delay_allreduce=True)
+
+    pretrained_model_path = best_checkpoint_folder / f'{calculation_name}.pth'
+    if pretrained_model_path.is_file():
+        state_dict = torch.load(pretrained_model_path, map_location=lambda storage, loc: storage)
+        model.load_state_dict(state_dict)
+
+        if distrib_config['LOCAL_RANK'] == 0:
+            fold_logger.info('load model from {}'.format(pretrained_model_path))
+
+    loss_fn = ChamferLoss()
+
+    if train_config['OPTIMIZER']['CLASS'] == 'RAdam':
+        optimizer_class = getattr(radam, train_config['OPTIMIZER']['CLASS'])
+    else:
+        optimizer_class = getattr(torch.optim, train_config['OPTIMIZER']['CLASS'])
+    optimizer = optimizer_class(model.parameters(), **train_config['OPTIMIZER']['ARGS'])
+
+    scheduler_class = getattr(torch.optim.lr_scheduler, train_config['SCHEDULER']['CLASS'])
+    scheduler = scheduler_class(optimizer, **train_config['SCHEDULER']['ARGS'])
+
+    n_epoches = train_config['EPOCHS']
+    accumulation_step = train_config['ACCUMULATION_STEP']
+    early_stopping = train_config['EARLY_STOPPING']
+
+    if distrib_config['LOCAL_RANK'] != 0:
+        fold_logger = None
+        fold_tb_logger = None
+
+    best_epoch, best_score = Learning(
+        distrib_config,
+        optimizer,
+        loss_fn,
+        device,
+        n_epoches,
+        scheduler,
+        accumulation_step,
+        early_stopping,
+        fold_logger,
+        fold_tb_logger,
+        best_checkpoint_folder,
+        checkpoints_history_folder,
+        checkpoints_topk,
+        calculation_name
+    ).run_train(model, train_dataloader, valid_dataloader, local_metric_fn, global_metric_fn)
+
+    fold_logger.info(f'Best Epoch : {best_epoch}, Best Score : {best_score}')
+
+
+if __name__ == '__main__':
+    args = argparser()
+    train_config = load_yaml(args.config_path)
+    distrib_config = {}
+    distrib_config['LOCAL_RANK'] = args.local_rank
+
+    root_dir = Path(train_config['DIRECTORY']['ROOT_DIRECTORY'])
+    data_dir = Path(train_config['DIRECTORY']['DATA_DIRECTORY'])
+    log_dir = root_dir / train_config['DIRECTORY']['LOGGER_DIRECTORY']
+    log_dir.mkdir(exist_ok=True, parents=True)
+
+    if distrib_config['LOCAL_RANK'] == 0:
+        main_logger = init_logger(log_dir, 'train_main.log')
+
+    torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.enabled = True
+    torch.backends.cudnn.deterministic = True
+    SEED = train_config['SEED']
+    seed_everything(SEED)
+    if distrib_config['LOCAL_RANK'] == 0:
+        main_logger.info(train_config)
+
+    if "DEVICE_LIST" in train_config:
+        os.environ["CUDA_VISIBLE_DEVICES"] = ','.join(map(str, train_config["DEVICE_LIST"]))
+
+    if len(train_config['DEVICE_LIST']) > 1:
+        distrib_config['DISTRIBUTED'] = True
+        torch.cuda.set_device(distrib_config['LOCAL_RANK'])
+        torch.distributed.init_process_group(backend='nccl',
+                                             init_method='env://')
+        distrib_config['WORLD_SIZE'] = torch.distributed.get_world_size()
+        train_config['OPTIMIZER']['ARGS']['lr'] = train_config['OPTIMIZER']['ARGS']['lr'] * float(
+            train_config['BATCH_SIZE'] * distrib_config['WORLD_SIZE']) / 256
+    else:
+        distrib_config['DISTRIBUTED'] = False
+        distrib_config['WORLD_SIZE'] = False
+
+    pipeline_name = train_config['PIPELINE_NAME']
+
+    pre_transform, transform = T.Compose([T.NormalizeScale()]), T.SamplePoints(1024)
+
+    num_workers = train_config['WORKERS']
+    batch_size = train_config['BATCH_SIZE']
+    n_folds = train_config['FOLD']['NUMBER']
+
+    usefolds = map(str, train_config['FOLD']['USEFOLDS'])
+    local_metric_fn, global_metric_fn = init_eval_fns(train_config)
+
+    for fold_id in usefolds:
+        if distrib_config['LOCAL_RANK'] == 0:
+            main_logger.info('Start training of {} fold....'.format(fold_id))
+
+        train_dataset = ModelNet(data_dir, '10', True,  transform, pre_transform)
+        valid_dataset = ModelNet(data_dir, '10', True,  transform, pre_transform)
+
+        if len(train_config['DEVICE_LIST']) > 1:
+            train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
+            valid_sampler = torch.utils.data.distributed.DistributedSampler(valid_dataset)
+
+            train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False,
+                                      sampler=train_sampler, num_workers=num_workers)
+            valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False,
+                                     sampler=valid_sampler, num_workers=num_workers)
+        else:
+            train_sampler = None
+            train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=(train_sampler is None),
+                                      num_workers=num_workers)
+            valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False,
+                                     num_workers=num_workers)
+
+        train_fold(
+            train_config, distrib_config, pipeline_name, log_dir,
+            fold_id, train_loader, valid_loader,
+            local_metric_fn, global_metric_fn
+        )
